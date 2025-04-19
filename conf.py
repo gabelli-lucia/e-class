3b34e0d5a37bd21c20232192018bb47a0909e71f
Federico Russo <federico.russo@antea.tech>
2025-04-19 12:49:35 +0200

    Formatta

--- conf.py
+++ conf.py
@@ -2,8 +2,7 @@ COL_CHECK = 'CHECK'
 COL_ID = 'Q01_ID-1'
 COL_MATRICOLA = 'Q02_Matricola'
 COL_TO_DROP = ['Q03_Course-3', 'Response', 'Submitted on:', 'Study Plan', 'Degree Code', 'Course', 'Group', 'ID',
-               'Full name',
-               'Username']
+               'Full name', 'Username']
 MAX_POINTS = 6
 COL_TO_INVERT = ['Q06_3', 'Q07_4', 'Q10_7', 'Q15_12', 'Q19_16', 'Q20_17', 'Q24_21', 'Q29_25', 'Q31_27',
                  'Q32_28', 'Q33_29']
